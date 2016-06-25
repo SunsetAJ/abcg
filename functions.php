0fe58b6f1f1985ddbc4b3a5bda3f58810c7a14b5
Adriaan <jvr.adriaan12@hotmail.com>
2016-06-25 21:00:06 +1000

    Column Select and Table Generate
    Set up the loop to show the column text areas and the inputs will be
    saved. Also intergrated the columns with the content area table view

--- functions.php
+++ functions.php
@@ -19,13 +19,19 @@
     * @param WP_Post $post Current post object.
   */
   function abcg_my_display_callback( $post ) {
+
+    $numAvailableCol = 12;
+
     wp_nonce_field(basename( __FILE__ ), 'acbg_nonce');
     $acbg_stored_meta = get_post_meta( $post->ID );
     ?>
+      <!-- Caption of the Meta Box -->
       <h3 style="margin-bottom: 0px;">Add a new Bootstrap Grid:</h3>
+      <small>* Once you have selected a NEW value press the update button *</small>
       <small style="margin-bottom: 15px; display: block;">* Bootstrap css & js is required for this to work *</small>
       <div class="form-elements">
 
+        <!-- Container Type -->
         <div style="margin-bottom: 20px;" class="container-type">
           <label for="containerType"><?php _e('Container Type *Default is normal*', 'treehouse_wp') ?></label>
           <select name="containerType" id="containerType">
@@ -34,6 +40,7 @@
           </select>
         </div>
 
+        <!-- Number of Rows -->
         <div style="margin-bottom: 20px;" class="num-of-rows">
           <label for="numOfRows"><?php _e('Number of Rows', 'treehouse_wp') ?></label>
           <select name="numOfRows" id="numOfRows">
@@ -44,9 +51,9 @@
               }
             ?>
           </select>
-          <small><br>* Once you have selected a NEW number press the update button *</small>
         </div>
 
+        <!-- Number of Columns - based of number of rows -->
         <?php
           $num = get_post_meta($post->ID, 'numOfRows', true);
 
@@ -60,18 +67,11 @@
                 <div class="<?php echo 'row-' . $x . '-columns' ?>">
                   <label for="<?php echo 'row-' . $x . '-columns' ?>"><?php _e('Row ' . $x . ' Columns', 'treehouse_wp') ?></label>
                   <select name="<?php echo 'row-' . $x . '-columns' ?>" id="<?php echo 'row-' . $x . '-columns' ?>">
-                    <option value="1" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], '1' ); ?>><?php _e( '1', 'treehouse_wp' )?></option>';
-                    <option value="2" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], '2' ); ?>><?php _e( '2', 'treehouse_wp' )?></option>';
-                    <option value="3" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], '3' ); ?>><?php _e( '3', 'treehouse_wp' )?></option>';
-                    <option value="4" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], '4' ); ?>><?php _e( '4', 'treehouse_wp' )?></option>';
-                    <option value="5" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], '5' ); ?>><?php _e( '5', 'treehouse_wp' )?></option>';
-                    <option value="6" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], '6' ); ?>><?php _e( '6', 'treehouse_wp' )?></option>';
-                    <option value="7" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], '7' ); ?>><?php _e( '7', 'treehouse_wp' )?></option>';
-                    <option value="8" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], '8' ); ?>><?php _e( '8', 'treehouse_wp' )?></option>';
-                    <option value="9" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], '9' ); ?>><?php _e( '9', 'treehouse_wp' )?></option>';
-                    <option value="10" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], '10' ); ?>><?php _e( '10', 'treehouse_wp' )?></option>';
-                    <option value="11" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], '11' ); ?>><?php _e( '11', 'treehouse_wp' )?></option>';
-                    <option value="12" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], '12' ); ?>><?php _e( '12', 'treehouse_wp' )?></option>';
+                    <?php
+                      for ($xy = 1; $xy <= $numAvailableCol; $xy++){
+                        ?> <option value="<?php echo $xy ?>" <?php if ( ! empty ( $acbg_stored_meta['row-' . $x . '-columns'] ) ) selected( $acbg_stored_meta['row-' . $x . '-columns'][0], $xy ); ?>><?php _e( $xy, 'treehouse_wp' )?></option>'; <?php
+                      }
+                    ?>
                   </select>
                 </div>
               <?php
@@ -82,42 +82,48 @@
           }
         ?>
 
+        <!-- Display Column Edit Area -->
+        <?php
+          if ($num >= 1){
+            ?>
+            <div style="margin-bottom: 20px;">
+            <h3 style="margin-bottom:0px;">Enter the Classes for each of the columns</h3>
+            <small style="margin-bottom: 15px; display: block;">* Separate Classes with a SPACE. These can also be custom classes *</small>
+            <?php
+            for ($i = 1; $i <= $num; $i++){
+              ${'row-' . $i . '-columnCount'} = get_post_meta($post->ID, 'row-' . $i . '-columns', true);
+              for ($iy = 1; $iy <= ${'row-' . $i . '-columnCount'}; $iy++){
+                ?>
+                  <label for="<?php echo 'row'.$i.'column'.$iy ?>"><?php _e('Row '. $i . ' Column ' . $iy . ' Class:', 'treehouse_wp') ?></label>
+                  <input type="text" name="<?php echo 'row'.$i.'column'.$iy ?>" value="<?php
+                    if (! empty($acbg_stored_meta['row'.$i.'column'.$iy.''])){
+                      echo esc_attr($acbg_stored_meta['row'.$i.'column'.$iy.''][0]);
+                    }
+                  ?>">
+                <?php
+                echo '<br>';
+              }
+              echo '<br>';
+            }
+            ?>
+            </div>
+            <?php
+          }
+        ?>
+
       </div>
     <?php
-    $content = '<table class="tg">
-                  <tr>
-                    <th class="tg-yw4l">1</th>
-                    <th class="tg-yw4l">1</th>
-                  </tr>
-                  <tr>
-                    <td class="tg-yw4l">2</td>
-                    <td class="tg-yw4l">2</td>
-                    <td class="tg-yw4l">2</td>
-                    <td class="tg-yw4l">2</td>
-                  </tr>
-                  <tr>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                  </tr>
-                  <tr>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                    <td class="tg-yw4l">3</td>
-                  </tr>
-                </table>';
+    $content = '<table class="abcg-table" style="width: 100%;">';
+                  $rows = 1;
+                  for($rowCount = 1; $rowCount <= $num; $rowCount++){
+                    $content .= '<tr class="row abgc-row-'.$rowCount.'">';
+                      ${'row-'.$rowCount.'-columnCount'} = get_post_meta($post->ID, 'row-'.$rowCount.'-columns', true);
+                      for ($colCount = 1; $colCount <= ${'row-'.$rowCount.'-columnCount'}; $colCount++){
+                        $content .= '<th>Column ' . $colCount . '</th>';
+                      }
+                    $content .= '</tr>';
+                  }
+                $content .= '</table>';
 
     wp_editor($content, 'table-area');
   }
@@ -127,6 +133,7 @@
   */
   function dwwp_meta_save( $post_id ) {
     $num = 6;
+    $numAvailableCol = 12;
     // Checks save status
     $is_autosave = wp_is_post_autosave( $post_id );
     $is_revision = wp_is_post_revision( $post_id );
@@ -151,6 +158,18 @@
         }
       }
     }
+
+    //This is also be interesting
+    if ($num >= 1){
+      for ($h = 1; $h <= $num; $h++){
+        for($hg = 1; $hg <= $numAvailableCol; $hg++){
+          if (isset($_POST['row'.$h.'column'.$hg.''])){
+            update_post_meta($post_id,'row'.$h.'column'.$hg.'', sanitize_text_field($_POST['row'.$h.'column'.$hg.'']));
+          }
+        }
+      }
+    }
+
   }
   add_action( 'save_post', 'dwwp_meta_save' );
 
