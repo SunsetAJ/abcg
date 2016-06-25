0814c995e1c3ef78c4657cee4c6ebe5cbc4e3ee3
Adriaan <jvr.adriaan12@hotmail.com>
2016-06-25 17:57:15 +1000

    Rows, Columns & Editor
    Users are now able to select the number of rows and columns in those
    rows. Also an example of entering custom content in a editor.

--- functions.php
+++ functions.php
@@ -26,17 +26,6 @@
       <small style="margin-bottom: 15px; display: block;">* Bootstrap css & js is required for this to work *</small>
       <div class="form-elements">
 
-<<<<<<< HEAD
-=======
-        <div style="margin-bottom: 20px;" class="test-field">
-          <label for="test_id"><?php _e('Test ID', 'treehouse_wp') ?></label>
-          <input type="text" name="test_id" id="test_id"
-  				value="<?php if ( ! empty ( $acbg_stored_meta['test_id'] ) ) {
-  					echo esc_attr( $acbg_stored_meta['test_id'][0] );
-  				} ?>"/>
-        </div>
-
->>>>>>> origin/master
         <div style="margin-bottom: 20px;" class="container-type">
           <label for="containerType"><?php _e('Container Type *Default is normal*', 'treehouse_wp') ?></label>
           <select name="containerType" id="containerType">
@@ -48,26 +37,17 @@
         <div style="margin-bottom: 20px;" class="num-of-rows">
           <label for="numOfRows"><?php _e('Number of Rows', 'treehouse_wp') ?></label>
           <select name="numOfRows" id="numOfRows">
-<<<<<<< HEAD
             <?php
               $rowCount = 6;
               for ($i = 0; $i <= $rowCount; $i++){
                 ?> <option value="<?php echo $i ?>" <?php if ( ! empty ( $acbg_stored_meta['numOfRows'] ) ) selected( $acbg_stored_meta['numOfRows'][0], $i ); ?>><?php _e( $i, 'treehouse_wp' )?></option>'; <?php
               }
             ?>
-=======
-            <option value="0" <?php if ( ! empty ( $acbg_stored_meta['numOfRows'] ) ) selected( $acbg_stored_meta['numOfRows'][0], '0' ); ?>><?php _e( '0', 'treehouse_wp' )?></option>';
-            <option value="1" <?php if ( ! empty ( $acbg_stored_meta['numOfRows'] ) ) selected( $acbg_stored_meta['numOfRows'][0], '1' ); ?>><?php _e( '1', 'treehouse_wp' )?></option>';
-            <option value="2" <?php if ( ! empty ( $acbg_stored_meta['numOfRows'] ) ) selected( $acbg_stored_meta['numOfRows'][0], '2' ); ?>><?php _e( '2', 'treehouse_wp' )?></option>';
-            <option value="3" <?php if ( ! empty ( $acbg_stored_meta['numOfRows'] ) ) selected( $acbg_stored_meta['numOfRows'][0], '3' ); ?>><?php _e( '3', 'treehouse_wp' )?></option>';
-            <option value="4" <?php if ( ! empty ( $acbg_stored_meta['numOfRows'] ) ) selected( $acbg_stored_meta['numOfRows'][0], '4' ); ?>><?php _e( '4', 'treehouse_wp' )?></option>';
->>>>>>> origin/master
           </select>
           <small><br>* Once you have selected a NEW number press the update button *</small>
         </div>
 
         <?php
-<<<<<<< HEAD
           $num = get_post_meta($post->ID, 'numOfRows', true);
 
           if ($num >= 1){
@@ -99,47 +79,78 @@
             ?>
               </div>
             <?php
-=======
-          $cont = get_post_meta($post->ID, 'containerType', true);
-          $num = get_post_meta($post->ID, 'numOfRows', true);
-        ?>
-
-        <?php
-          if ($num >= 1){
-            
->>>>>>> origin/master
           }
         ?>
 
       </div>
     <?php
+    $content = '<table class="tg">
+                  <tr>
+                    <th class="tg-yw4l">1</th>
+                    <th class="tg-yw4l">1</th>
+                  </tr>
+                  <tr>
+                    <td class="tg-yw4l">2</td>
+                    <td class="tg-yw4l">2</td>
+                    <td class="tg-yw4l">2</td>
+                    <td class="tg-yw4l">2</td>
+                  </tr>
+                  <tr>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                  </tr>
+                  <tr>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                    <td class="tg-yw4l">3</td>
+                  </tr>
+                </table>';
+
+    wp_editor($content, 'table-area');
   }
 
   /**
   * @param int $post_id Post ID
   */
   function dwwp_meta_save( $post_id ) {
-  	// Checks save status
-      $is_autosave = wp_is_post_autosave( $post_id );
-      $is_revision = wp_is_post_revision( $post_id );
-      $is_valid_nonce = ( isset( $_POST[ 'acbg_nonce' ] ) && wp_verify_nonce( $_POST[ 'acbg_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
-      // Exits script depending on save status
-      if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
-          return;
-      }
+    $num = 6;
+    // Checks save status
+    $is_autosave = wp_is_post_autosave( $post_id );
+    $is_revision = wp_is_post_revision( $post_id );
+    $is_valid_nonce = ( isset( $_POST[ 'acbg_nonce' ] ) && wp_verify_nonce( $_POST[ 'acbg_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+    // Exits script depending on save status
+    if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+        return;
+    }
 
-      if (isset($_POST['containerType'])){
-          update_post_meta($post_id, 'containerType', sanitize_text_field($_POST['containerType']));
+    //Saves each of the input fields
+    if (isset($_POST['containerType'])){
+        update_post_meta($post_id, 'containerType', sanitize_text_field($_POST['containerType']));
+    }
+    if ( isset( $_POST['numOfRows'])){
+      update_post_meta($post_id, 'numOfRows', sanitize_text_field($_POST['numOfRows']));
+    }
+    //This will be interesting
+    if ($num >= 1){
+      for ($y = 1; $y <= $num; $y++){
+        if ( isset( $_POST['row-' . $y . '-columns'])){
+          update_post_meta($post_id, 'row-' . $y . '-columns', sanitize_text_field($_POST['row-' . $y . '-columns']));
+        }
       }
-      if ( isset( $_POST['numOfRows'])){
-        update_post_meta($post_id, 'numOfRows', sanitize_text_field($_POST['numOfRows']));
-      }
-      if (isset($_POST['containerType'])){
-          update_post_meta($post_id, 'containerType', sanitize_text_field($_POST['containerType']));
-      }
-      if ( isset( $_POST['numOfRows'])){
-        update_post_meta($post_id, 'numOfRows', sanitize_text_field($_POST['numOfRows']));
-      }
+    }
   }
   add_action( 'save_post', 'dwwp_meta_save' );
 
